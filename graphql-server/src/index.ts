c5325454481c181b17f2ffdaf405979a97fb2a6e
charles <luck.goldragon@gmail.com>
2024-06-11 15:49:03 -0400

    version 2

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,3 +1,29 @@
 // src/index.ts
 
-console.log('Hey there! 👋');
+import { createServer } from "node:http";
+import { createSchema, createYoga } from 'graphql-yoga'
+import { schema } from "./schema";
+
+const port = Number(process.env.API_PORT) || 4000
+
+const yoga = createYoga({
+    // schema: createSchema({
+    //   typeDefs: /* GraphQL */ `
+    //     type Query {
+    //       hello: String
+    //     }
+    //   `,
+    //   resolvers: {
+    //     Query: {
+    //       hello: () => 'Hello from Yoga!'
+    //     }
+    //   }
+    // })
+    schema: schema
+  })
+
+const server = createServer(yoga)
+
+server.listen(4000, () => {
+    console.info('Server is running on http://localhost:4000/graphql')
+})
---
+++ graphql-server/src/models/Messages.ts
@@ -0,0 +1,13 @@
+// src/models/Messages.ts
+
+import { builder } from "../builder"
+
+builder.prismaObject("Message", {
+    fields: (t) => ({
+        id: t.exposeID("id"),
+        body: t.exposeString("body"),
+        createdAt: t.expose("createdAt", {
+            type: "Date",
+        }),
+    }),
+});
